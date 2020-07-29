23757ba7c71fda238c3becd14ed9c833916b1a3d
Jim <jim@pointybracket.net>
2020-07-29 01:04:26 -0500

    new branch

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def main(msg):
-    // Adding a second message
-    // Print a message
-
+    // No comment needed
     print(msg)
+
+def secondary():
+    print("new function")
 
 main ("Hello People")
 
